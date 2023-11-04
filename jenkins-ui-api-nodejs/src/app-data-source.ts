cef22f6748c3bcd15233e12cd102138bca42f6a4
Fabio Sbano <fsbano@yahoo.com>
2023-11-04 12:14:39 -0300

    modified:   jenkins-ui-api-nodejs/.env 	modified:   jenkins-ui-api-nodejs/src/app-data-source.ts

--- jenkins-ui-api-nodejs/src/app-data-source.ts
+++ jenkins-ui-api-nodejs/src/app-data-source.ts
@@ -2,20 +2,19 @@ import { JobDetailsEntity } from './job-details.entity';
 import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "postgres",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "postgres",
-    synchronize: true,
-    entities: [
-      JobDetailsEntity,
-    ],
-  })
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_DATABASE || "postgres",
+  synchronize: process.env.DB_SYNCHRONIZE === "true", // Be cautious with this in production!
+  logging: process.env.DB_LOGGING === "true",
+  entities: [JobDetailsEntity],
+});
   
-  AppDataSource.initialize();
+AppDataSource.initialize();
 
-  module.exports = {
-    AppDataSource
-  }
+module.exports = {
+  AppDataSource
+}
