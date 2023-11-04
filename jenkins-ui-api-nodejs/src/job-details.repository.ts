6014f83aaf13827cfabc2e81dc19c48e96ae3a30
Fabio Sbano <fsbano@yahoo.com>
2023-11-04 10:02:08 -0300

    modified:   jenkins-ui-api-nodejs/src/job-details.entity.ts 	modified:   jenkins-ui-api-nodejs/src/job-details.repository.ts

--- jenkins-ui-api-nodejs/src/job-details.repository.ts
+++ jenkins-ui-api-nodejs/src/job-details.repository.ts
@@ -2,4 +2,4 @@ import { EntityRepository, Repository } from 'typeorm';
 import { JobDetails } from './job-details.entity';
 
 @EntityRepository(JobDetails)
-export class JobDetailsRepositoryRepository extends Repository<JobDetailsRepository> {}
+export class JobDetailsRepositoryRepository extends Repository<JobDetailsEntity> {}
